d608be59e4b8ad5e8bbde6c34e184d5e17083f5a
Maarten Beeckmans <maartenb@inuits.eu>
2021-01-22 22:27:17 +0100

    initial commit

---
+++ update_module.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+module_name='choria-mcollective_choria'
+module_version='0.20.0'
+
+echo 'Updating module'
+
+wget -c "https://forge.puppet.com/v3/files/${module_name}-${module_version}.tar.gz" -O - | tar -xz
+
+mv ${module_name}-${module_version}/* .
+
+rm -rfv "${module_name}-${module_version}"
